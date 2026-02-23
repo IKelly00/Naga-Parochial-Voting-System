8566117bba4b17851350ac087a5e2ad53cc48d14
Michael Roy A. Jardinel <https://github.com/IKelly00>
2026-02-23 20:43:08 +0800

    Deleted some assets

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from "react";
 import "./Header.css";
 import Logo from "../../assets/Logo.jpg";
-
 
 const Header = () => {
   return (
     <>
-        <header>
-          <div className="img-container">
-            <img src={Logo} className="logo" width="50" height="50"></img>
-            <p className="school-name">Naga Parochial School</p>
-          </div>
-          <div className="nav-links">
-            <a id='voteNow' href="#">Vote Now</a>
-          </div>
-        </header>
+      <header>
+        <div className="img-container">
+          <img src={Logo} className="logo" width="50" height="50"></img>
+          <p className="school-name">Naga Parochial School</p>
+        </div>
+        <div className="nav-links">
+          <a id="voteNow" href="#">
+            Vote Now
+          </a>
+        </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
